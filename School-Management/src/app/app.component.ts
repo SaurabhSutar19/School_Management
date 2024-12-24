98e6cc4f42d4948afa32a61019589b5f7cb89a45
Saurabh19 <saurabhsutar0620@gmail.com>
2024-12-24 19:11:38 +0530

    import to login page class path

--- School-Management/src/app/app.component.ts
+++ School-Management/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'School-Management';
